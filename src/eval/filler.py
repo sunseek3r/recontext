df53924b9d57d332f60b200c355e418a0b17c383
Pavlo <pavlo.pasko.main@gmail.com>
2026-03-14 20:54:46 +0100

    Add filler

---
+++ src/eval/filler.py
@@ -0,0 +1,112 @@
+
+import json
+import random
+import string
+
+from openai import OpenAI
+
+import sacrebleu
+from src.create_context import create_context_for_repo
+
+from pathlib import Path
+
+def complete_middle(prefix, suffix, context_str):
+    """
+    Placeholder for a call to an LLM to complete the middle.
+    """
+    # For now, return a fixed string.
+    MODEL_ID = "./models/Qwen3-Coder-30B-A3B-Instruct-UD-Q4_K_XL.gguf"
+
+    client = OpenAI(base_url="http://localhost:8000/v1", api_key="EMPTY")
+
+
+    full_prompt = (
+        f"<|fim_prefix|>{context_str}\n"
+        f"// Current file:\n"
+        f"{prefix}<|fim_suffix|>{suffix}<|fim_middle|>"
+    )
+
+    response = client.completions.create(
+        model=MODEL_ID,
+        prompt=full_prompt,
+        max_tokens=512,    # completions are usually short
+        temperature=0.0,   # stay precise for coding
+        # Stop tokens prevent the model from continuing into the suffix code
+        stop=["<|file_separator|>", "<|endoftext|>", "\n\n"]
+    )
+
+    return response.choices[0].text
+
+def calculate_chrf_jsonl(predictions_file: str, references_file: str, output_file: str):
+    with open(predictions_file, 'r') as f_preds, \
+        open(references_file, 'r') as f_refs, \
+        open(output_file, 'w') as f_out:
+
+        for line_num, (pred, ref) in enumerate(zip(f_preds, f_refs)):
+            try:
+                data_p = json.loads(pred)
+                data_r = json.loads(ref)
+
+                comp_p = data_p.get('middle', '')
+                comp_r = data_r.get('middle', '')
+
+
+                chrf_metric = sacrebleu.sentence_chrf(comp_p, [comp_r])
+                score = chrf_metric.score
+
+                print(f"Line: {line_num} | score: {score:.2f}")
+
+                output_data = {
+                    "line_number": line_num,
+                    "score": score,
+                    "prediction": comp_p,
+                    "reference": comp_r
+                }
+                f_out.write(json.dumps(output_data) + "\n")
+            except json.JSONDecodeError:
+                print(f"Line {line_num:03d} | Error: Invalid JSON format. Skipping.")
+            except Exception as e:
+                print(f"Line {line_num:03d} | Error: {e}")
+
+
+def evaluate_filler(file_path, answers_path):
+    input_path = Path(file_path).name
+
+    out_path = f'predictions/{input_path}'
+    Path(out_path).parent.mkdir(parents=True, exist_ok=True)
+
+    res_path = f'results/{input_path}'
+    Path(res_path).parent.mkdir(parents=True, exist_ok=True)
+
+    with open(file_path, 'r') as input_file, \
+        open(f'{out_path}', 'w') as pred_file, \
+            open(f'{res_path}', 'w') as res_file:
+            for line in input_file:
+                data = json.loads(line)
+
+                repo_name = data['repo'].replace('/', '__') + '-' + data['revision']
+
+                file_prefix = data['prefix']
+                file_suffix = data['suffix']
+
+
+                context_str = create_context_for_repo(repo_name, file_prefix, file_suffix)
+                data['context'] = context_str
+
+
+                middle = complete_middle(file_prefix, file_suffix, context_str)
+
+                pred_file.write(json.dumps({'middle': middle}) + '\n')
+                res_file.write(json.dumps(data) + '\n')
+    
+
+    calculate_chrf_jsonl(
+        predictions_file=f'predictions/{file_path}',
+        references_file=answers_path
+    )
+
+    
+    
+
+if __name__ == '__main__':
+    evaluate_filler('data/python-practice.jsonl', 'data/answers-python-practice.jsonl')
